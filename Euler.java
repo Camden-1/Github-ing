745d2f1753570495cf5acf697c6f10cdef26930b
Mo1013 <mohsayem13@gmail.com>
2022-01-15 12:00:00 -0500

    Added project Euler Problem One

---
+++ Euler.java
@@ -0,0 +1,10 @@
+public class Euler
+{
+    public static void main(String[] args) {
+        int x = 0;
+        long z = 600851475143;
+      for( int i = z; i > 0; i--)
+        if( z % i == 0);
+        
+    }
+}
